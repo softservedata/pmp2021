2615f0e61a13b56daee5c82d47108885264a3284
bebebe <hahaha@gmail.com>
2022-02-17 11:06:36 +0200

    example homework02

--- pmp11alg/App.cpp
+++ pmp11alg/App.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main()
+int main01()
 {
 	int a, b, c, max;
 	cout << "a = ";
---
+++ pmp11alg/Homework02.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\ndone02" << endl;
+	system("pause");
+	return 0;
+}
