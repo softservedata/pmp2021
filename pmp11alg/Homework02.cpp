5bb27be586519763bdc643e993649baf8ce8ab31
bebebe <hahaha@gmail.com>
2022-02-17 11:16:30 +0200

    Search maximum

--- pmp11alg/Homework02.cpp
+++ pmp11alg/Homework02.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main()
 {
+	double a = 0;
+	double b = 0;
+	double max = 0;
+	//
+	cout << " a = ";
+	cin >> a;
+	cout << " b = ";
+	cin >> b;
+	if (a > b)
+	{
+		max = a;
+	}
+	else
+	{
+		max = b;
+	}
+	cout << " max = " << max << endl;
+	//
 	cout << "\ndone02" << endl;
 	system("pause");
 	return 0;
