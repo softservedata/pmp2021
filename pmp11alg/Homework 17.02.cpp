cb675c010c3c1bb3029ad9f7cd95eadd74488ad7
Artem <partemrserb@gmail.com>
2022-02-17 11:12:58 +0200

    File updated

---
+++ pmp11alg/Homework 17.02.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	short a, b, c;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	cout << endl;
+	if (a >= b && a >= c)
+	{
+		if (b <= c)
+		{
+			cout << b << ' ' << c << ' ' << a << endl;
+		}
+		else if (b >= c)
+		{
+			cout << c << ' ' << b << ' ' << a << endl;
+		}
+	}
+	else if (a <= b && a <= c)
+	{
+		if (b <= c)
+		{
+			cout << a << ' ' << b << ' ' << c << endl;
+		}
+		else if (b >= c)
+		{
+			cout << a << ' ' << c << ' ' << b << endl;
+		}
+	}
+	else if (a > b && a < c)
+	{
+		cout << b << ' ' << a << ' ' << c << endl;
+	}
+	else if (a < b && a > c)
+	{
+		cout << c << ' ' << a << ' ' << b << endl;
+	}
+	else if (a == b == c)
+	{
+		cout << a << ' ' << b << ' ' << c << endl;
+	}
+	return 0;
+}
