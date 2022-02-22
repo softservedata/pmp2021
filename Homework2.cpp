b915d9864d2eac210bcf9da0a2d8152e36d31b1f
SofiaSeniv <90135240+SofiaSeniv@users.noreply.github.com>
2022-02-22 23:43:16 +0200

    Add files via upload

---
+++ Homework2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int year;
+	cout << "year - ";
+	cin >> year;
+	if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+	{
+		cout << "This year is leap" << endl;
+	}
+	else
+	{
+		cout << "This year isn't leap" << endl;
+	}
+
+
+
+
+
+
+
+	return 0;
+}
