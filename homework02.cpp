8edc101f00726a324279472a6240f5ea65f2aebc
MarichkaDenys <99760032+MarichkaDenys@users.noreply.github.com>
2022-02-23 20:13:39 +0200

    Add files via upload

---
+++ homework02.cpp
@@ -0,0 +1,24 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	int year;
+	cout << "Enter a number ";
+	cin >> year;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0))
+		{
+			cout << "Leap year" << endl;
+		}
+	}
+
+	else {
+		cout << "No leap year" << endl;
+	}
+
+
+	return 0;
+	system("pause");
+}*/ 
