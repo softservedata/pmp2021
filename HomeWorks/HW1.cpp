adfd0458db75c3c27c738f5dc51747618b5de4e2
Artur Chekalin <arturchkekal60@gmail.com>
2022-03-17 10:40:21 +0200

    HW

---
+++ HomeWorks/HW1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main01()
+{
+	int a, b, c;
+	cout << "a=";
+	cin >> a;
+	cout << "b=";
+	cin >> b;
+	/* //A
+	c=a;
+	a=b;
+	b=c;*/
+
+	/* //B
+	a+=b;
+	b=a-b;
+	a-=b;*/
+
+	/* //C
+	a*=b;
+	b=a/b;
+	a/=b;*/
+	return 0;
+
+}
