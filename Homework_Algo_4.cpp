323b9f7db304668a2cc3cb6c3e30ee12413599b3
Vladkutsyk <vladkutsyk@gmail.com>
2022-03-23 23:57:07 +0200

    Added Homework

---
+++ Homework_Algo_4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+void reading(int** arr, int n, int m);
+void print(int** arr, int n ,int m);
+void sumus(int** arr1, int** arr2 ,int** arr3, int** arr4, int n, int m);
+int main(){
+    int n,m;
+    cout<<"n="; cin>>n;
+    cout<<"m="; cin>>m;
+    int** A= new int*[n];
+    for (int i = 0; i < n; i++)
+        A[i] = new int[m];
+    int** B= new int*[n];
+    for (int i = 0; i < n; i++)
+        B[i] = new int[m];
+    int** C= new int*[n];
+    for (int i = 0; i < n; i++)
+        C[i] = new int[m];
+    int** D= new int*[n];
+    for (int i = 0; i < n; i++)
+        D[i] = new int[m];
+    cout<<"mass A:"<<endl;
+    reading(A, n, m);
+    cout<<"mass B:"<<endl;
+    reading(B, n, m);
+    cout<<"mass C:"<<endl;
+    reading(C, n, m);
+    sumus(D, A, B, C, n, m);
+    cout<<"mass D:"<<endl;
+    print(D,n,m);
+}
+void reading(int** arr, int n, int m){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << "elem[" << i << "]" << "[" << j << "] = ";
+            cin >> *(*(arr+i)+j);
+        }
+    }
+}
+void print(int** arr, int n ,int m){
+    for(int i=0; i<n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout<<*(*(arr+i)+j)<<"\t";
+        }
+        cout<<"\n";
+    }
+}
+void sumus(int** arr1, int** arr2 ,int** arr3, int** arr4, int n, int m){
+    for(int i=0; i<n; i++) {
+        for (int j = 0; j < m; j++){
+            *(*(arr1+i)+j)=*(*(arr2+i)+j)+*(*(arr3+i)+j)+*(*(arr4+i)+j);
+        }
+    }
+}
