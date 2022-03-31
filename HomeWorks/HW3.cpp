c5f8dec38da21cfc41a9cdeaeb7557abe1b439f1
Artur Chekalin <arturchkekal60@gmail.com>
2022-03-31 10:07:07 +0300

    Update HW3.cpp

--- HomeWorks/HW3.cpp
+++ HomeWorks/HW3.cpp
@@ -3,17 +3,25 @@ using namespace std;
 
 int main()
 {
-int n,sum=1,temp_sum=0,count1=2,count2=2;
-cout << "n=";
-cin >> n;
-for(int i=2;i<=n;i++){
-temp_sum+=i;
-if(count1>count2){
-    sum*=temp_sum;
-    temp_sum=0;
-    count2=count1;
-}
-count1++;
-}
-
+    int n, sum = 1, temp_sum = 0, count = 0, l = 1;
+    cout << "n=";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (count == l) {
+            sum *= temp_sum;
+            cout << "tem_sum1 = " << temp_sum << " i=" << i << endl;
+            temp_sum = 0;
+            count = 0;
+            l++;
+        }
+        if (i == n) {
+            temp_sum += i;
+            sum *= temp_sum;
+            cout << "tem_sum2 = " << temp_sum << " i=" << i << endl;
+        }
+        count++;
+        temp_sum += i;
+        cout << "tem_sum4 = " << temp_sum << " i=" << i<< endl;
+    }
+    cout << "sum=" << sum;
 }
