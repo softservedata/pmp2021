fc2c1b1b399df96e46d1a7544f056eb3c4a06f06
Artur Chekalin <53403561+ArturChk@users.noreply.github.com>
2022-03-31 10:20:42 +0300

    Create HW3_2.cpp

---
+++ HomeWorks/HW3_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n);
+
+int main()
+{
+    int n, sum = 0, factor_sum, count = 0, l = 1;
+    cout << "n=";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        factor_sum = fact(i);
+        sum += factor_sum;
+    }
+    cout << "sum=" << sum;
+ 
+}
+
+int fact(int n) {
+    int fact_sum = 1;
+    for (int i = 1; i <= n; i++) {
+        fact_sum *= i;
+    }
+    return fact_sum;
+
+}
