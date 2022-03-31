785827296e0668d72d1d086a06eab94841bc0055
ivayanc <ivanyanchenko.odb@gmail.com>
2022-03-31 14:41:17 +0200

    homework2022_03_24 done

---
+++ homework2022_03_24.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Ivan Yanchenko on 31.03.2022.
+//
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//task 1 b
+double sum(int n, int started_n){
+    double tmp = sin(n);
+    for(int i = n; i < started_n; ++i){
+        tmp = sin(tmp);
+    }
+    if(n == 1){
+        return tmp;
+    }
+    return tmp + sum(n - 1, started_n);
+}
+
+//task 1 a
+double suma(int & n) {
+    double sum = 0, tmp;
+    for (int i = n; i >= 1; --i) {
+        tmp = sin(i);
+        for (int j = i; j < n; ++j) {
+            tmp = sin(tmp);
+        }
+        sum += tmp;
+    }
+    return sum;
+}
+
+//task 2
+int second_sum(int n){
+    if(n <= 0){
+        return 0;
+    }
+    return n + second_sum(n - 2);
+}
+
+int main () {
+    int n;
+    double ans;
+    cin >> n;
+    ans = suma(n);
+    cout << ans << "\n";
+    return 0;
+}
