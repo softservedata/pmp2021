3dee1fccb853a04441a756dbff36db23e594188b
Влада Монастирська <99765557+VladaMonasturska@users.noreply.github.com>
2022-04-03 16:52:15 +0300

    Add file

---
+++ Homework06_1.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+/*
+double** read_matrix(int& n)
+{
+    double** matrix = new double* [n];
+    for (int i = 0; i < n; ++i) {
+        *(matrix + i) = new double[n];
+        for (int j = 0; j < n; ++j) {
+            cin >> *(*(matrix + i) + j);
+        }
+    }
+    cout << endl;
+    return matrix;
+}
+*/
+double** read_matr(int n)
+{
+    double** matr = new double* [n];
+    for (int i = 0; i < n; ++i)
+    {
+        matr[i] = new double[n];
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> matr[i][j];
+        }
+    }
+    return matr;
+}
+void print_matrix(double** matrix, int& n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << *(*(matrix + i) + j) << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+/* 
+double** subtraction_matrices(double** matrix1, double** matrix2, int& n) {
+    double** matrix = new double* [n];
+    for (int i = 0; i < n; ++i) {
+        *(matrix + i) = new double[n];
+        for (int j = 0; j < n; ++j) {
+            *(*(matrix + i) + j) = *(*(matrix1 + i) + j) - *(*(matrix2 + i) + j);
+        }
+    }
+    return matrix;
+}
+*/
+double** zeros(int n) {
+    double** matr = new double* [n];
+    for (int i = 0; i < n; ++i) {
+        matr[i] = new double[n];
+        for (int j = 0; j < n; ++j) {
+            matr[i][j] = 0;
+        }
+    }
+    return matr;
+}
+
+
+double** sum_matr(double** matr1, double** matr2, int n) {
+    double** matr = zeros(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matr[i][j] = matr1[i][j] - matr2[i][j] ;
+        }
+    }
+    return matr;
+}
+double** multiply_matrices(double** matrix1, double** matrix2, int& n) {
+    double** matrix = new double* [n], sum = 0;
+    for (int i = 0; i < n; ++i) {
+        *(matrix + i) = new double[n];
+        for (int j = 0; j < n; ++j) {
+            for (int l = 0; l < n; ++l) {
+                sum += matrix1[i][l] * matrix2[l][j];
+            }
+            matrix[i][j] = sum;
+            sum = 0;
+        }
+    }
+    return matrix;
+}
+
+void delet(double** matr, int n) {
+    for (int i = 0; i < n; ++i) {
+        delete[] matr[i];
+    }
+    delete[] matr;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter n= ";
+	cin >> n;
+    cout << "Matr A"<<endl;
+
+    double** A = read_matr(n);
+
+    cout << "Matr B"<<endl;
+    double** B = read_matr(n); 
+    double** C = sum_matr(A, B, n);
+    double** P = multiply_matrices(C, C, n);
+    print_matrix(P, n);
+    delet(A, n);
+    delet(B, n);
+    delet(C, n);
+    delet(P, n);
+    
+	
+}
