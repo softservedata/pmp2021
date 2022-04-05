195bb894c017ae1df9ba9788051e5c789a7c2f48
Artur Chekalin <arturchkekal60@gmail.com>
2022-04-05 22:18:57 +0300

    s

--- HomeWorks/HW5_1_a.cpp
+++ HomeWorks/HW5_1_a.cpp
@@ -11,7 +11,7 @@ int main() {
     cin >> n;
     k = n;
     cout << "sin1 " << sin(1) << endl << "sin2 " << sin(sin(1)) + sin(2) << endl << "sin3 " << sin(sin(sin(1))) + sin(sin(2)) + sin(3) << endl;
-    cout << "sin4 " << sin(sin(sin(sin(1)))) + sin(sin(sin(2))) + sin(sin(3)) + sin(4)<< endl;
+    cout << "sin4 " << sin(sin(sin(sin(1)))) + sin(sin(sin(2))) + sin(sin(3)) + sin(4)<< endl<< "sin5 "<<sin(sin(sin(sin(sin(1)))))+sin(sin(sin(sin(2))))+sin(sin(sin(3)))+sin(sin(4))+sin(5) << endl;
     cout << "rec_sin = " <<rec_sin2(n,k) << endl;
     system("PAUSE");
     return 0;
