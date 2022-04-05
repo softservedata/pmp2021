b85101c966814da1c57e3eb7a12fc07e1728864e
Artur Chekalin <arturchkekal60@gmail.com>
2022-04-05 16:43:30 +0300

    ss

--- HomeWorks/HW2.cpp
+++ HomeWorks/HW2.cpp
@@ -6,11 +6,11 @@ int main(){
     cout << "year : ";
     cin >> year;
     if(year%4==0){
-        if(year%100==0 && year%400==0) cout << "A leap year";
-        else if(year%100!=0) cout << "A leap year";
-
-    }
-    else cout << "Is not a leap year";
+        if(year%100==0 && year%400==0) cout << "A leap year\n";
+        else if(year%100!=0) cout << "A leap year\n";
+        else cout << "Is not a leap year\n";
+}
+    else cout << "Is not a leap year\n";
     system("PAUSE");
     return 0;
 }
