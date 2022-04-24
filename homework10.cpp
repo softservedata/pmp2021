400277157d89a94c9486c1e00e7c17936857899d
Denys Gusti <90290236+DenysGusti@users.noreply.github.com>
2022-04-24 19:45:55 +0300

    updated

--- homework10.cpp
+++ homework10.cpp
@@ -7,43 +7,23 @@ using namespace std;
 
 vector<vector<bool>> incidence_matrix;
 vector<bool> visited;
-vector<int> tin, low; // entry time for nodes; bridge checking
+vector<int> disc, low; // час відкриття вершин; масив для перевірки мостів
 vector<pair<int, int>> bridges;
 int timer = 0;
 
 void dfs(int v, int p = -1) {
     visited[v] = true;
-    tin[v] = low[v] = timer++;
-
-/*    cout << "\ndfs: from " << p << " to " << v << '\n';
-    cout << "visited: ";
-    for (auto el: visited)
-        cout << el;
-    cout << '\n';
-    cout << "tin: ";
-    for (auto &el: tin)
-        cout << el << ' ';
-    cout << '\n';
-    cout << "low: ";
-    for (auto &el: low)
-        cout << el << ' ';
-    cout << '\n';*/
-
+    disc[v] = low[v] = timer++;
     for (int to = 0; to < incidence_matrix.size(); ++to)
         if (incidence_matrix[v][to]) { // equivalent to: for (auto &to: adjacency_list[v])
-            if (to == p) // loop
+            if (to == p) // не вертаємося до батьківської вершини
                 continue;
-            // low[v] = min(tin[v], tin[p] for all p for which (v, p) is a back edge (цикл),
-            //                      low[to] for all to for which (v, to) is a tree edge (каркас))
-            if (visited[to]) {
-//                cout << "here visited to\t" << v << ' ' << to << ": " << low[v] << ' ' << tin[to] << '\n';
-                low[v] = min(low[v], tin[to]);
-            } else {
+            if (visited[to])
+                low[v] = min(low[v], disc[to]); // якщо утворився цикл, всім цим вершинам присвоїти час входження першої
+            else {
                 dfs(to, v);
-                low[v] = min(low[v], low[to]);
-/*                cout << "here not visited to\t" << v << ' ' << to << ": " << low[v] << ' ' << low[to] << '\n';
-                cout << low[to] << ' ' << tin[v] << '\n';*/
-                if (low[to] > tin[v]) // < a back edge from v or descendants to ancestors, =  back edge directly to v
+                low[v] = min(low[v], low[to]); // порівнюємо з дочірнім вузлом бектрекінгом
+                if (low[to] > disc[v]) // умова моста, дочірня не з'єднувалася з предками v
                     bridges.emplace_back(min(v, to), max(v, to));
             }
         }
@@ -51,19 +31,17 @@ void dfs(int v, int p = -1) {
 
 void find_bridges() {
     visited.assign(incidence_matrix.size(), false);
-    tin.assign(incidence_matrix.size(), -1);
+    disc.assign(incidence_matrix.size(), -1);
     low.assign(incidence_matrix.size(), -1);
-    bridges.reserve(incidence_matrix.size() - 1); // max n - 1 bridges for trees
-    for (int i = 0; i < incidence_matrix.size(); ++i) { // if disconnected
-        if (!visited[i]) {
-//            cout << "\ndfs from: " << i << '\n';
+    bridges.reserve(incidence_matrix.size() - 1); // max n - 1 для дерев
+    for (int i = 0; i < incidence_matrix.size(); ++i) { // якщо ліс
+        if (!visited[i])
             dfs(i);
-        }
     }
 }
 
-// Для заданого зв’язного графа знайти всі мости. Граф представити за допомогою матриці суміжності.
-// Мостом в графі називається ребро, за відсутності якого граф перестає бути зв’язним.
+// Для заданого зв'язного графа знайти всі мости. Граф представити за допомогою матриці суміжності.
+// Мостом в графі називається ребро, за відсутності якого граф перестає бути зв'язним.
 
 int main() {
     size_t n{}, m{};
@@ -74,7 +52,7 @@ int main() {
     for (int tmp1, tmp2, i = 0; i < m; ++i) {
         cin >> tmp1 >> tmp2;
         incidence_matrix[tmp1][tmp2] = true;
-        incidence_matrix[tmp2][tmp1] = true; // undirected
+        incidence_matrix[tmp2][tmp1] = true; // неорієнтований
     }
 
     for (const auto &row: incidence_matrix) {
@@ -84,19 +62,6 @@ int main() {
     }
 
     find_bridges();
-
-/*    cout << "visited: ";
-    for (auto el: visited)
-        cout << el;
-    cout << '\n';
-    cout << "tin: ";
-    for (auto &el: tin)
-        cout << el << ' ';
-    cout << '\n';
-    cout << "low: ";
-    for (auto &el: low)
-        cout << el << ' ';
-    cout << '\n';*/
 
     ranges::sort(bridges);
     cout << "bridges:\n";
