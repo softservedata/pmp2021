11102fa8f9c43b969416f09d84263865c00afc46
NataliaAndreiko <90283129+NataliaAndreiko@users.noreply.github.com>
2022-05-20 12:46:06 +0300

    Homework

--- pmp11alg/Homework_2022_04_28.cpp
+++ pmp11alg/Homework_2022_04_28.cpp
@@ -8,19 +8,18 @@ template<typename T>
 std::ostream& operator << (std::ostream& os, const std::set<T>& s);
 
 int main(){
-    std::set<size_t> set1 = { 1, 2, 3, 4, 5 };
-    std::set<size_t> set2 = { 2, 4, 6, 7 };
+    std::set<size_t> first_set = { 1, 2, 3, 4, 5 };
+    std::set<size_t> second_set = { 2, 4, 6, 7 };
 
     std::set<size_t> set_un;
     std::set<size_t> set_inter;
 
-    set_union(begin(set1), end(set1), begin(set2), end(set2), inserter(set_un, end(set_un)));
-    set_intersection(begin(set1), end(set1), begin(set2), end(set2), inserter(set_inter, end(set_inter)));
+    set_union(begin(first_set), end(first_set), begin(second_set), end(second_set), inserter(set_un, end(set_un)));
+    set_intersection(begin(first_set), end(first_set), begin(second_set), end(second_set), inserter(set_inter, end(set_inter)));
 
-    std::cout << "sets for example\nX = { " << set1 << " }\nY = { " << set2 << " }" << std::endl;
+    std::cout << "sets for example\nX = { " << first_set << " }\nY = { " << second_set << " }" << std::endl;
     std::cout << "Result of union of sets: " << set_un << std::endl;
     std::cout << "Resul of intersection of sets: " << set_inter << std::endl;
-
 }
 
 template<typename T>
