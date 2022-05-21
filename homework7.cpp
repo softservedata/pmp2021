9053fefff240c1f4a3737ff9b06ffa3a4b0feb33
vladzakaliuk <99800065+vladzakaliuk@users.noreply.github.com>
2022-05-22 00:57:48 +0300

    Update homework7.cpp

--- homework7.cpp
+++ homework7.cpp
@@ -1,3 +1,41 @@
+1)
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << " n = ";
+    cin >> n ;
+    int a[n][n];
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cin >> a[i][j];
+        }
+    }
+    int b[n][n];
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            b[i][j] = a[j][i];
+        }
+    }
+    cout<< "Transposed matrix : "<<endl;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}	
+
+
+
+
+
+
+
+
+2)
+	
 #include <iostream>
 #include <math.h>
 using namespace std;
